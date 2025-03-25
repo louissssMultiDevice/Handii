ef9ce718e123139855f785c248016312ab0a8b55
louissssMultiDevice <theboyscuyy@gmail.com>
2025-03-25 16:22:00 +0700

    Create handii.js

---
+++ handii.js
@@ -0,0 +1,12 @@
+const text = "Hai, Selamat Datang di Fika Store!";
+let index = 0;
+
+function typeEffect() {
+    if (index < text.length) {
+        document.getElementById("typing-text").textContent += text.charAt(index);
+        index++;
+        setTimeout(typeEffect, 100);
+    }
+}
+
+typeEffect();
